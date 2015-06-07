c8b048f5266efac6be56b43f8ea85d38e10b9fc7
Christopher Stokes <christopherstokes@gmail.com>
2015-06-06 20:40:54 -0700

    refactor and add db

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 var cloudinary = require('cloudinary');
+var fs = require('fs');
+
+// file is included here:
+eval(fs.readFileSync('config.js')+'');
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
+});
 
 app.use(express.static(__dirname + '/public'));
 
