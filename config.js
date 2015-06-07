a0f9e3a682ecf00fd69b34bbdd89d588e7531a08
Christopher Stokes <christopherstokes@gmail.com>
2015-06-06 20:43:15 -0700

    add config

---
+++ config.js
@@ -0,0 +1,7 @@
+cloudinary.config({
+	cloud_name: 'tacotime24seven-us',
+	api_key: '346676967257841',
+	api_secret: 'nV122QcF0b4OAZ_IjVwhA4An5tU'
+});
+
+var url = 'mongodb://tacos:k1ll3m4ll@ds043012.mongolab.com:43012/tacotime';
